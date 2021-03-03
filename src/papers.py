591bf47e7d28c04dbbffc8ce260a40ce1e01e3f1
Waffloid <waffloidrblx@gmail.com>
2021-03-03 21:29:19 +0000

    Removed print spam

--- src/papers.py
+++ src/papers.py
@@ -2,7 +2,6 @@ def filter(pool, papers):
     newPool = []
 
     for i, question_tuple in enumerate(pool):
-        print(question_tuple[0], (question_tuple[0] in papers))
         if (question_tuple[0] in papers):
             newPool.append(question_tuple)
     
