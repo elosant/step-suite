fc00b4cd39cc5971b950d82909cf6a739f17804c
Waffloid <waffloidrblx@gmail.com>
2021-03-03 21:16:08 +0000

    Fixed papers.py
    The --paper command didn't actually filter for selected papers.

--- src/papers.py
+++ src/papers.py
@@ -1,5 +1,9 @@
 def filter(pool, papers):
+    newPool = []
+
     for i, question_tuple in enumerate(pool):
-        if not (question_tuple[0] in papers):
-            pool.pop(i)
-    return pool
+        print(question_tuple[0], (question_tuple[0] in papers))
+        if (question_tuple[0] in papers):
+            newPool.append(question_tuple)
+    
+    return newPool
